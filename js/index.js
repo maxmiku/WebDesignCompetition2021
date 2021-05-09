542763a739d0ce6ef42cf843aae949ff1d92d94d
maxmiku <jmchenlx@outlook.com>
2021-05-10 01:21:34 +0800

    玩家能动了

--- js/index.js
+++ js/index.js
@@ -12,3 +12,44 @@ function onDocKeydown(e)
    }
 }
 document.onkeydown = onDocKeydown;
+
+$(function(){
+	
+	$('.boxT').on('click',function(e){
+		console.log('helo',e)
+		spot=getBoxPosition($('.boxPlayer'))
+		tpot=getBoxPosition(e.currentTarget);
+		console.log('Player Move',spot,'->',tpot)
+		movePlayer(tpot[0],tpot[1])
+
+	})
+})
+
+// 获取指定盒子的位置
+/*
+定位规则
+
+0,0---1,0----2,0----3,0----> x
+ \
+ 0,1
+   \
+   0,2
+     \
+	 0,3
+	   \
+	   ↓   y
+
+
+*/
+function getBoxPosition(box){
+	console.log($(box).parent().parent().index(),$(box).parent().parent().parent().index());
+	pot=[$(box).parent().parent().index(),$(box).parent().parent().parent().index()]
+	return pot;
+}
+
+function movePlayer(x,y){
+	$('.boxPlayer').removeClass('boxPlayer');
+	$($($('.box').children()[y]).find('.boxT')[x]).addClass('boxPlayer');
+
+}
+
